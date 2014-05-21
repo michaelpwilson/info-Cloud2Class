54a3ad6745d727d68628b52564fd2a4e482dc1d7
Michael Wilson <michaelpwilson96@gmail.com>
2014-05-21 16:21:32 +0100

    fixed bugs

--- includes/intro-header.php
+++ includes/intro-header.php
@@ -5,8 +5,10 @@
     <div class="span4">
       <img class="intro-logo" src="img/c2c-logo.png"/>
       <div class="content-heading"><h3>The Simple and Effective cloud learning environment</h3></div>
+	<div class="intro-pHolder">
       <p class="intro-paragraph">The Cloud2Class platform provides destructible computing environments for students to learn and experiment with, all available through a web-browser.</p>
 	<p class="intro-paragraph">Give whole classes easy access to console-based virtual computers. Put the power of the cloud in to your classroom.</p>
+        </div>
     </div>
   </div>
 </div>
